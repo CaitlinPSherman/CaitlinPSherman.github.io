a32c4f9b1c45885f58f890cf2775794a7369e01d
Caitlin Pauze Sherman <caitlinpSherman@gmail.com>
2022-09-08 18:06:26 -0400

    nav menu is responsive w hamburger in small view

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Contact = () => {
   return (
     <div className='section' id='contact'>
+      <h2>Get in touch!</h2>
       <p>here's contact info</p>
     </div>
   );
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,20 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Logo
- from './Logo';
+import React, { useState } from 'react';
+import Logo from './Logo';
+
 const Nav = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
   return (
     <div className="nav">
+      <a
+        href="#home"
+        onClick={() => {
+          setIsNavExpanded(false);
+        }}
+      >
         <Logo />
-      <div id="nav-links">
-        <a href='#bio'>About</a>
-        <a href='#projects'>Projects</a>
-        <a href='#skills'>Skills</a>
-        <a href='#contact'>Contact</a>
-        {/* <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/skills">Skills</Link> */}
+      </a>
+      <button
+        className="hamburger"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="#e91e63"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
+      <div
+        className={isNavExpanded ? 'nav-links expanded' : 'nav-links'}
+        onClick={() => {
+          setIsNavExpanded(false);
+        }}
+      >
+        <a href="#bio">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#skills">Skills</a>
+        <a href="#contact">Contact</a>
       </div>
     </div>
   );
