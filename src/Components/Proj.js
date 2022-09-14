d748df0f3cf6789dd5c817d8c7ca8d9125d34136
Caitlin Pauze Sherman <caitlinpSherman@gmail.com>
2022-09-14 17:58:41 -0400

    project info passed to Proj components via props

--- src/Components/Proj.js
+++ src/Components/Proj.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React from 'react';
 
-const Proj = () => {
-    return (
-        <p>here's single project</p>
-    )
-}
+const Proj = ({ projData }) => {
+  return (
+    <div className='project'>
+      <h3>{projData.name}</h3>
+      <p>{projData.description}</p>
+      <a href='{projData.url}'>Link</a>
+      <a href='{projData.github}'>Github</a>
+    </div>
+  );
+};
 
 export default Proj;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,13 +2,41 @@ import React from 'react';
 import Proj from './Proj';
 
 const Projects = () => {
+  const projects = [
+    {
+      name: "Bell's Custom Worx",
+      url: 'https://bells-custom-worx.herokuapp.com/',
+      github: 'https://github.com/GH2202-Team-Worx/Worx',
+      description: 'An ecom site for a family-owned woodworking business.',
+    },
+    {
+      name: 'Pictures Drawn Poorly',
+      url: 'https://pictures-drawn-poorly.herokuapp.com/',
+      github: 'https://github.com/CaitlinPSherman/stackathon',
+      description: 'A game to make confusingly-bad drawings with your friends. Still a work in progress!',
+    },
+    {
+      name: 'Creature Coders',
+      url: 'https://creature-coders.herokuapp.com/',
+      github: 'https://github.com/pigeon-programmers/creature-coders-web',
+      description:
+        'A learn-to-code game designed for the iPad. Learn the basics of programming concepts and Javascript fundamentals while you collect fun hats for your garbage animal friends.',
+    },
+    {
+      name: "Caitlin's Portfolio",
+      url: 'caitlinpsherman.com',
+      github: 'https://github.com/CaitlinPSherman/portfolio',
+      description:
+        "The website you're looking at right now! My personal portfolio, to show off the various projects I've made.",
+    },
+  ];
+
   return (
-    <div className='section' id='projects'>
-      <h2>here's projects page</h2>
-      <Proj />
-      <Proj />
-      <Proj />
-      <Proj />
+    <div className="section" id="projects">
+      <h2>Projects</h2>
+      {projects.map((proj, index) => (
+        <Proj key={index} projData={proj} />
+      ))}
     </div>
   );
 };
