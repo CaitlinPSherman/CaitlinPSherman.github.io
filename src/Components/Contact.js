fc16f0a18258cd2795783bcbb02337fcd3634060
Caitlin Pauze Sherman <caitlinpSherman@gmail.com>
2022-10-13 13:32:20 -0400

    updated Contact to add icons

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -47,23 +47,41 @@ const Contact = () => {
         </svg>
       ),
     },
+    {
+      name: 'Resume',
+      url: '',
+      svg: (
+        <svg
+          className="contact-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="40"
+          viewBox="0 0 24 24"
+        >
+          <path d="M11.362 2c4.156 0 2.638 6 2.638 6s6-1.65 6 2.457v11.543h-16v-20h7.362zm.827-2h-10.189v24h20v-14.386c0-2.391-6.648-9.614-9.811-9.614zm4.811 13h-10v-1h10v1zm0 2h-10v1h10v-1zm0 3h-10v1h10v-1z" />
+        </svg>
+      ),
+    },
   ];
+  //{name: '', url: '', svg: ()}
 
   return (
     <div className="section" id="contact">
       <h2>Get in touch!</h2>
-      <ul>
+      <div className="contact-container">
         {contact.map((c) => {
           return (
-            <li>
+            <div className="contact-item">
               <a href={c.url} target="_blank" rel="noreferrer noopener">
                 {c.svg}
+              </a>
+              <a href={c.url} target="_blank" rel="noreferrer noopener">
                 {c.name}
               </a>
-            </li>
+            </div>
           );
         })}
-      </ul>
+      </div>
     </div>
   );
 };
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import headshot from '../app/images/Caitlin_pic.jpg'
+import headshot from '../app/images/Caitlin_pic.jpg';
 
 const Home = () => {
   return (
-    <div className='section' id='home'>
-
+    <div className="section" id="home">
+      <div className="headshot-container">
+        <img src={headshot} alt="headshot" className="headshot" />
+      </div>
       <p>Hello!</p>
-      <img src={headshot} alt='headshot' id='headshot'/>
       <h2>I'm Caitlin.</h2>
-      <p>I make stuff for the internet - welcome to my own little chunk of it.</p>
+      <p>
+        I make stuff for the internet - welcome to my own little chunk of it.
+      </p>
     </div>
   );
 };
