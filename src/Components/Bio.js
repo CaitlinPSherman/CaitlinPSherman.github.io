61ab004cdcf58c4a6cb2d4e9d5e7d1f149fd2c31
Caitlin Pauze Sherman <caitlinpSherman@gmail.com>
2022-10-13 21:12:27 -0400

    added project image files and favicon, updated bio text

--- src/Components/Bio.js
+++ src/Components/Bio.js
@@ -7,23 +7,27 @@ const Bio = () => {
       <p>
         I recently graduated from the Grace Hopper program at Fullstack Academy.
         Before my shift into software engineering, I was an apparel designer for
-        15 years, focused primarily on mass market menswear and print design.
-        When I decided to make a career change last year I originally considered
-        data visualization design but I ended up enjoying the programming part
-        of that so much that I decided to become a software engineer instead!
+        15 years, focused primarily on mass market menswear and print design. I
+        loved creating products for people to use in their everyday lives, but I
+        felt it was time for a change and wanted to move into an industry with
+        more innovation and growth. I originally considered data visualization
+        design but I ended up enjoying the programming part of that so much that
+        I decided to become a software engineer instead!
       </p>
       <p>
         My past jobs gave me experience with managing complex projects from
-        conception through delivery, communicating clearly with people from a
-        wide range of backgrounds and across language and timezone barriers, and
-        a desire to use new tools and software to work more efficiently. I enjoy
-        using my creative and problem-solving skills to build beautiful and
-        user-friendly web applications. I am looking forward to becoming a
-        front-end or full stack engineer within a company that is working to
-        make the world a better place.
+        conception through delivery; communicating clearly with people from a
+        wide range of backgrounds and across language and timezone barriers; and
+        a desire to use new tools and software to work more efficiently. At
+        Grace Hopper I learned exciting new frameworks and computer science
+        concepts, and the thrill of learning and working with my classmates in
+        an agile environment. I enjoy using my creative and problem-solving
+        skills to build beautiful and user-friendly web applications. I am
+        looking forward to becoming a front-end or full stack engineer within a
+        company that is working to make the world a better place.
       </p>
       <p>
-        In my spare time I love knitting, hiking, bird-watching, spoiling my two
+        In my spare time I love hiking, knitting, bird-watching, spoiling my two
         cats, and forgetting mugs of tea around the house.
       </p>
     </div>
